8b628f41ee312716160a270224be165dfd52c063
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-09-10 13:10:15 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Part_1_3_Chapter_Assingment
+{
+    partial class BasicInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblResult2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(114, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(301, 174);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(115, 20);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Transparent;
+            this.btnSubmit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSubmit.CausesValidation = false;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSubmit.Image = global::Part_1_3_Chapter_Assingment.Properties.Resources._61QKhYL_eCL;
+            this.btnSubmit.Location = new System.Drawing.Point(430, 59);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(145, 89);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit!";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.BackColor = System.Drawing.Color.Transparent;
+            this.lblGreeting.Font = new System.Drawing.Font("MS PGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreeting.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGreeting.Location = new System.Drawing.Point(262, 32);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(64, 24);
+            this.lblGreeting.TabIndex = 4;
+            this.lblGreeting.Text = "Hello!";
+            this.lblGreeting.Click += new System.EventHandler(this.lblGreeting_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblName.Location = new System.Drawing.Point(60, 83);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.BackColor = System.Drawing.Color.Transparent;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAge.Location = new System.Drawing.Point(69, 130);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 6;
+            this.lblAge.Text = "Age";
+            this.lblAge.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHeight.Location = new System.Drawing.Point(69, 177);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(226, 13);
+            this.lblHeight.TabIndex = 7;
+            this.lblHeight.Text = "Height in metres to two decimal places";
+            this.lblHeight.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblInstruction.Location = new System.Drawing.Point(239, 59);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(113, 13);
+            this.lblInstruction.TabIndex = 8;
+            this.lblInstruction.Text = "Please input your..";
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(114, 128);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(78, 20);
+            this.numAge.TabIndex = 9;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResult.Location = new System.Drawing.Point(60, 234);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 10;
+            // 
+            // lblResult2
+            // 
+            this.lblResult2.AutoSize = true;
+            this.lblResult2.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResult2.Location = new System.Drawing.Point(60, 261);
+            this.lblResult2.Name = "lblResult2";
+            this.lblResult2.Size = new System.Drawing.Size(0, 13);
+            this.lblResult2.TabIndex = 11;
+            // 
+            // BasicInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::Part_1_3_Chapter_Assingment.Properties.Resources.Design_Guide_IO_2X1;
+            this.ClientSize = new System.Drawing.Size(608, 368);
+            this.Controls.Add(this.lblResult2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblGreeting);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtName);
+            this.Name = "BasicInput";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblResult2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_1_3_Chapter_Assingment
+{
+    public partial class BasicInput : Form
+    {
+        public BasicInput()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGreeting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name;
+            int age;
+            double height;
+
+            name = txtName.Text;
+            age = Convert.ToInt32(numAge.Text);
+            height = Convert.ToDouble(txtHeight.Text);
+
+            lblResult.Text = "Hi " + name + ", you are " + (82 - age) + " years under the average life expectancy and you are ";
+            lblResult2.Text = (2.72 - height) + " metres shorter than the tallest man alive!";
+
+        }
+    }
+}
